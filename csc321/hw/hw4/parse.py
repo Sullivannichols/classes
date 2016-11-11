f854e9147a495240fcffdfe1663e54dae98756ca
Brandon Randle <brandonkrandle@gmail.com>
2016-11-11 10:37:48 -0600

    added notes for lab setup, modified parsing

--- csc321/hw/hw4/parse.py
+++ csc321/hw/hw4/parse.py
@@ -2,17 +2,14 @@
 
 import csv
 import socket
-import networkx as nx
-import matplotlib.pyplot as plt
-
-# LOOK AT urllib.parse for parsing the urls from the reverse lookups
+import pygraphviz as pgv
 
 def domains():
     ''' This function extracts domain names from a .tsv file, performs a
         forward DNS lookup on each domain, performs a reverse DNS lookup on
         each returned IP, then adds all of the info to a graph.
     '''
-    g = nx.Graph()
+    g = pgv.AGraph()
     with open('domains.tsv', newline='') as tsvfile:
         tsvin = csv.DictReader(tsvfile, delimiter='\t')
         print("domains.tsv loaded into dictionary") 
@@ -25,7 +22,6 @@ def domains():
                 ips = socket.gethostbyname_ex(domain)            
             except socket.gaierror:
                 print("socket.gaierror: nodename nor servname provided")
-                #print('FORWARD DNS ERROR for' + domain) 
                 continue
 
             # Reverse Lookup
@@ -38,12 +34,17 @@ def domains():
                     noerror = False
                     continue
                 if noerror:
+                    # parse domain names
+                    result = result.split(".")
+                    result = ".".join(len(result[-2]) < 4 and\
+                        result[-3:] or result[-2:])
+                    # add nodes
                     g.add_node(result)
                     g.add_edge(domain, result)
                     print("node added for " + result)
                     print("edge added between " + domain + " and " + result)
-    nx.draw(g, with_labels=True)
-    plt.show()
+    g.layout(args='-Goverlap=false')
+    g.draw('simple.png')
 
 if __name__ == '__main__':
     domains()
