728906c8d119b155d8b666d428ef15d75d66f96b
Brandon Randle <brandonkrandle@gmail.com>
2016-11-10 20:42:52 -0600

    updated parsing script

--- csc321/hw/hw4/parse.py
+++ csc321/hw/hw4/parse.py
@@ -5,45 +5,45 @@ import socket
 import networkx as nx
 import matplotlib.pyplot as plt
 
-class DNSGraph:
+# LOOK AT urllib.parse for parsing the urls from the reverse lookups
 
-    def __init__(self):
-        self.domainfile = 'domains.tsv'
-        self.graph = nx.Graph()
-        
-    def domains(self):
-        ''' This function extracts domain names from a .tsv file, performs 
-            a forward DNS lookup for the domain, then performs a reverse 
-            DNS lookup for each IP gained from the forward DNS lookup, then
-            stores the information in a FORMAT TO BE DETERMINED.
-        '''
-        with open(self.domainfile, newline='') as tsvfile:
-            tsvin = csv.DictReader(tsvfile, delimiter='\t')
-            for line in tsvin:
-                # Forward Lookup
-                # ADD NODE FOR DOMAIN
+def domains():
+    ''' This function extracts domain names from a .tsv file, performs a
+        forward DNS lookup on each domain, performs a reverse DNS lookup on
+        each returned IP, then adds all of the info to a graph.
+    '''
+    g = nx.Graph()
+    with open('domains.tsv', newline='') as tsvfile:
+        tsvin = csv.DictReader(tsvfile, delimiter='\t')
+        print("domains.tsv loaded into dictionary") 
+        for line in tsvin:
+            # Forward Lookup
+            domain = line['domain']
+            g.add_node(domain)
+            print("node added for " + domain)
+            try:
+                ips = socket.gethostbyname_ex(domain)            
+            except socket.gaierror:
+                print("socket.gaierror: nodename nor servname provided")
+                #print('FORWARD DNS ERROR for' + domain) 
+                continue
+
+            # Reverse Lookup
+            for item in ips[2]:
+                noerror = True
                 try:
-                    ips = socket.gethostbyname_ex(line['domain'])            
+                    result = socket.getfqdn(item)
                 except:
-                    print('FORWARD DNS ERROR') 
+                    print('REVERSE DNS ERROR for ' + result)
+                    noerror = False
                     continue
-
-                # Reverse Lookup
-                try:
-                    for item in ips[2]:
-                        result = socket.gethostbyaddr(item)
-                        # PARSE RESULT
-                        # ADD NODES FOR EACH DOMAIN ASSOCIATED WITH IP
-                except:
-                    print('REVERSE DNS ERROR')
-                    continue
-
-    def graph():
-        ''' This function graphs the data gained from domains() '''
-        nx.draw(G)
-        plt.show()
+                if noerror:
+                    g.add_node(result)
+                    g.add_edge(domain, result)
+                    print("node added for " + result)
+                    print("edge added between " + domain + " and " + result)
+    nx.draw(g, with_labels=True)
+    plt.show()
 
 if __name__ == '__main__':
-    g = DNSGraph()
-    g.domains()
-    g.graph
+    domains()
