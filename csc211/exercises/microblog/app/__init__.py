7432702081658d9b8880877df09bd5c6eb37ad49
Brandon Randle <brandonkrandle@gmail.com>
2016-11-17 08:06:42 -0600

    updated microblog

--- csc211/exercises/microblog/app/__init__.py
+++ csc211/exercises/microblog/app/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQAlchemy(app)
 
-from app import views
+from app import views, models
