774fcdd68948b6e5a864017707bfbe8a58171675
Brandon Randle <brandonkrandle@gmail.com>
2016-11-29 11:07:22 -0600

    added database stuff

--- csc211/exercises/microblog/app/__init__.py
+++ csc211/exercises/microblog/app/__init__.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_log import LoginManager
+from flask_openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
--- csc211/exercises/microblog/app/views.py
+++ csc211/exercises/microblog/app/views.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request
+from flask import g
+from flask_log import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
+from .models import User
 
 @app.route('/')
 @app.route('/index')
@@ -21,14 +24,20 @@ def index():
     return render_template('index.html',title='Home',user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-            (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html', 
-                           title='Sign In', 
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html',
+                           title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS']
                            )
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
